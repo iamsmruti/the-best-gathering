4d9adb1be4d23a38965b191196e9ff486b342e75
Gyana Ranjan Panda <grp.gyanaranjan@gmail.com>
2022-10-04 17:05:21 +0530

    custom textfield

---
+++ src/textfields/gyana-ranjan/GyanaCustomFields.js
@@ -0,0 +1,10 @@
+import { TextField } from '@mui/material'
+import React from 'react'
+
+const GyanaCustomFields = (props) => {
+  return (
+    <TextField {...props} sx={{maxWidth: '280px'}}/>
+  )
+}
+
+export default GyanaCustomFields
