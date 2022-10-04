34d75506b9321652d7e3f1ba04b226fd8c1e112a
Smruti Ranjan Badatya <smrutiranjanbadatya2@gmail.com>
2022-10-04 18:10:16 +0530

    updated states

--- src/buttons/smruti-ranjan/SmrutiButton.js
+++ src/buttons/smruti-ranjan/SmrutiButton.js
@@ -2,9 +2,9 @@ import { GitHub } from '@mui/icons-material'
 import { Button } from '@mui/material'
 import React from 'react'
 
-const SmrutiButton = () => {
+const SmrutiButton = ({text, link}) => {
   return (
-    <Button endIcon={<GitHub />} href={''} variant='contained' sx={{mr: 2, mb: 1}}>made by Smruti</Button>
+    <Button endIcon={<GitHub />} href={link} variant='contained' sx={{mr: 2, mb: 1}}>{text}</Button>
   )
 }
 
