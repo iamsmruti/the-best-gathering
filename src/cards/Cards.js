0c364b35d24f6bd926b87d4e26eed9605ad41d43
Sai Vaishnav <vaishnavsai01@gmail.com>
2022-10-05 19:47:24 +0530

    added my card

--- src/cards/Cards.js
+++ src/cards/Cards.js
@@ -4,6 +4,7 @@ import { Stack, Typography } from '@mui/material'
 import React from 'react'
 
 import Card from './smruti-ranjan/SmrutiCard'
+import Card2 from './vaishnav/Card2'
 
 const Cards = () => {
   return (
@@ -13,6 +14,17 @@ const Cards = () => {
                 <Card />
                 <Stack direction={'row'} sx={{pt: 1}} alignItems={'center'} justifyContent={'space-around'}>
                     <Typography variant="caption">Contributed By Smruti Ranjan</Typography>
+                    <IconButton>
+                        <GitHub />
+                    </IconButton>
+                </Stack>
+            </Box>
+        </Grid>
+        <Grid md={3} xs={12}>
+            <Box sx={{width: 'fit-content', mr: 2, mb: 3}}>
+                <Card2 />
+                <Stack direction={'row'} sx={{pt: 1}} alignItems={'center'} justifyContent={'space-around'}>
+                    <Typography variant="caption">Contributed By Sai Vaishnav</Typography>
                     <IconButton>
                         <GitHub />
                     </IconButton>
---
+++ src/cards/vaishnav/Card2.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { blue, red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+export default function RecipeReviewCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: blue[500] }} aria-label="recipe">
+            S
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Sai Vaishnav"
+        subheader="January 24, 2001"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image="https://t4.ftcdn.net/jpg/04/95/28/65/360_F_495286577_rpsT2Shmr6g81hOhGXALhxWOfx1vOQBa.jpg"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          H3llo World
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+}
