0ada4127b66627efeb2454c6aa1137b5da20ffd6
Gyana Ranjan Panda <grp.gyanaranjan@gmail.com>
2022-10-05 13:43:51 +0530

    custom btn

--- src/buttons/Buttons.js
+++ src/buttons/Buttons.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Stack } from '@mui/material'
 import SmrutiButton from './smruti-ranjan/SmrutiButton'
 import SaiButton from './sai-kiran/SaiButton'
+import Custombtn from './gyana-ranjan/Custombtn'
 
 const Buttons = () => {
   return (
-    <Stack direction={'row'} sx={{flexWrap: 'wrap', p: 2}} justifyContent={'center'}>
-        <SmrutiButton text={'Smruti Ranjan Badatya'} link={'https://github.com/iamsmruti'}/>
-        <SaiButton text={'Sai Kiran Mansingh'} link={'https://github.com/skms625'}/>
+    <Stack direction={'row'} sx={{ flexWrap: 'wrap', p: 2 }} justifyContent={'center'}>
+      <SmrutiButton text={'Smruti Ranjan Badatya'} link={'https://github.com/iamsmruti'} />
+      <SaiButton text={'Sai Kiran Mansingh'} link={'https://github.com/skms625'} />
+      <Custombtn text='Meditating Panda' link='https://github.com/Meditatingpanda' />
     </Stack>
   )
 }
