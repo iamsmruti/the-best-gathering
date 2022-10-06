cf166374308f6c97a7da532080f0984c49fe08e4
Rohit984-hash <rohitmohanty1688@gmail.com>
2022-10-06 13:10:49 +0530

    Update MaterialUI.js

--- src/icons/MaterialUI.js
+++ src/icons/MaterialUI.js
@@ -8,6 +8,7 @@ import AgricultureIcon from '@mui/icons-material/Agriculture';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import AddReactionIcon from '@mui/icons-material/AddReaction';
+import AddModeratorIcon from '@mui/icons-material/AddModerator';
 const MaterialUI = () => {
   return (
     <Box>
@@ -34,6 +35,9 @@ const MaterialUI = () => {
             </Box>
             <Box sx={{p: 1}}>
                 <  AddReactionIcon  sx={{fontSize: 'large'}}/>
+            </Box>
+            <Box sx={{p: 1}}>
+                <  AddModeratorIcon   sx={{fontSize: 'large'}}/>
             </Box>
         </Stack>
     </Box>
