79636a46a2cdaf68d60ead9e961cf535fbd5b988
Swastik Mishra <78549150+SwastikMishra26@users.noreply.github.com>
2022-10-14 23:09:48 +0530

    Update MaterialUI.js

--- src/icons/MaterialUI.js
+++ src/icons/MaterialUI.js
@@ -87,6 +87,9 @@ const MaterialUI = () => {
             <Box sx={{p: 1}}>
                 <  AcUnitIcon   sx={{fontSize: 'large'}}/>
             </Box>
+             <Box sx={{p: 1}}>
+                <  AcUnitIcon   sx={{fontSize: 'large'}}/>
+            </Box>
         </Stack>
     </Box>
   )
