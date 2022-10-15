845e8d26d18a18973d2eab8748ee4129f9ca8462
SAI KIRANMAN SINGH <mansingh23me@gmail.com>
2022-10-15 12:56:57 +0530

    added my tbutton

---
+++ src/toggleButton/sai-kiran/ToggleButton.js
@@ -0,0 +1,15 @@
+import { Stack } from '@mui/system'
+import React from 'react'
+import SaiToggle from './smruti-ranjan/SmrutiToggle'
+
+const ToggleButton = () => {
+  return (
+    <Stack sx={{p: 6}}>
+      <SaiToggle />
+    </Stack>
+  )
+}
+
+export default ToggleButton
+
+
