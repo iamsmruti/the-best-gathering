049d57c985a38e9657c2d59a4fdd649d27fa2107
Chandan2610 <115131410+Chandan2610@users.noreply.github.com>
2022-10-23 20:16:37 +0530

    Create toggle.js

---
+++ src/toggleButton/anon/toggle.js
@@ -0,0 +1,22 @@
+
+import React from 'react'
+import 'rsuite/dist/styles/rsuite-default.css';
+import { Toggle } from 'rsuite';
+
+export default function App() {
+
+// State for the current toggle value
+const [currentValue, setCurrentValue] = React.useState(0)
+
+return (
+	<div style={{
+	display: 'block', width: 400, paddingLeft: 30
+	}}>
+	<h4>React Suite Toggle Component</h4>
+	<Toggle
+		onChange={(value) => { setCurrentValue(value) }}
+	/>
+	{currentValue === true ? "Toggle On" : "Toggle Off"}
+	</div>
+);
+}
